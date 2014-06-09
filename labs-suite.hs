b0e938a489a701b7ea6c51eb39af6752e323692d
David Ko <david.dk.ko@gmail.com>
2014-06-09 14:15:41 -0700

    cabal-ified everything

--- labs-suite.hs
+++ labs-suite.hs
@@ -5,6 +5,7 @@ import System.FilePath ((</>))
 import Control.Concurrent
 import qualified Data.Text as T
 import System.Process
+import System.Environment
 
 linkbotLabs :: FilePath -> IO ()
 linkbotLabs directory = do
@@ -24,11 +25,15 @@ choosePort = return 3000
 startServer :: Port -> FilePath -> IO ()
 startServer port dir = run port app
   where
-    app :: Application -- aka Request -> IO Response
-    app req =
+    app :: Application -- aka Request -> (Reponse -> IO ReponseReceived) -> IO ResponseReceived
+    app req func =
       let path = foldl (</>) dir $ map T.unpack $ pathInfo req
       in
-        return $ responseFile status200 [] path Nothing
+        func $ responseFile status200 [] path Nothing
 
 startBrowser port = fmap (const ()) $ rawSystem "./barobobrowser" [url]
   where url = "http://localhost:" ++ show port ++ "/index.html"
+
+main = do
+    args <- getArgs
+    linkbotLabs $ head args
